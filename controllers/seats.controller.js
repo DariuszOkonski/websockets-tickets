412bd4d775739ec5773c950fa1562490f5b2f2a3
Dariusz Okoński <darek200180@gmail.com>
2025-09-12 08:16:35 +0200

    add controller fo seats

---
+++ controllers/seats.controller.js
@@ -0,0 +1,128 @@
+const Seats = require('../models/seat.model');
+const mongoose = require('mongoose');
+
+const getAll = async (req, res) => {
+  try {
+    const seats = await Seats.find();
+    return res.json({ data: seats });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+};
+
+const getSeat = async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: 'Invalid testimonial ID' });
+    }
+
+    const seat = await Seats.findById(req.params.id);
+
+    if (!seat) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+
+    return res.json({ data: seat });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+};
+
+const createSeat = async (req, res) => {
+  try {
+    if (!req.body) {
+      return res.status(400).json({ message: 'missing all data' });
+    }
+
+    if (
+      !req.body.day ||
+      !req.body.seat ||
+      !req.body.client ||
+      !req.body.email
+    ) {
+      return res.status(400).json({ message: 'missing some data' });
+    }
+
+    const seats = await Seats.find();
+
+    const isSeatAlreadyTaken = seats.some(
+      ({ seat, day }) =>
+        seat === Number(req.body.seat) && day === Number(req.body.day)
+    );
+
+    if (isSeatAlreadyTaken) {
+      return res.status(409).json({ message: 'The slot is already taken...' });
+    }
+
+    const { day, seat, client, email } = req.body;
+
+    const newSeat = new Seats({ day, seat, client, email });
+    newSeat.save();
+
+    req.io.emit('seatsUpdated', db.seats);
+
+    res.json({ message: 'OK' });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+};
+
+const updateSeat = async (req, res) => {
+  try {
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({ message: 'missing data to update' });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: 'Invalid testimonial ID' });
+    }
+
+    const foundedSeat = await Seats.findById(req.params.id);
+
+    if (!foundedSeat) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+
+    const day = req.body.day ?? foundedSeat.day;
+    const seat = req.body.seat ?? foundedSeat.seat;
+    const client = req.body.client ?? foundedSeat.client;
+    const email = req.body.email ?? foundedSeat.email;
+
+    await Seats.updateOne(
+      { _id: req.params.id },
+      { $set: { day, seat, client, email } }
+    );
+
+    return res.json({ message: 'OK' });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+};
+
+const deleteSeat = async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: 'Invalid department ID' });
+    }
+
+    const seat = await Seats.findById(req.params.id);
+
+    if (!seat) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+
+    await Seats.deleteOne({ _id: req.params.id });
+
+    return res.json({ message: 'OK' });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+};
+
+module.exports = {
+  getAll,
+  getSeat,
+  createSeat,
+  updateSeat,
+  deleteSeat,
+};
--- routes/seats.js
+++ routes/seats.js
@@ -1,103 +1,16 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
-// const socketIO = require('socket.io');
-let db = require('../db');
 
-router.get('/', (req, res) => {
-  res.json({ data: db.seats });
-});
+const SeatController = require('../controllers/seats.controller');
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const seats = db.seats.find((item) => item.id == id);
+router.get('/', SeatController.getAll);
 
-  if (seats) {
-    return res.json({ data: seats });
-  } else {
-    return res.status(404).json({ message: 'not found' });
-  }
-});
+router.get('/:id', SeatController.getSeat);
 
-router.post('/', (req, res) => {
-  if (!req.body) {
-    return res.status(400).json({ message: 'missing all data' });
-  }
+router.post('/', SeatController.createSeat);
 
-  if (!req.body.day || !req.body.seat || !req.body.client || !req.body.email) {
-    return res.status(400).json({ message: 'missing some data' });
-  }
+router.put('/:id', SeatController.updateSeat);
 
-  const isSeatAlreadyTaken = db.seats.some(
-    ({ seat, day }) => seat === req.body.seat && day === req.body.day
-  );
-
-  if (isSeatAlreadyTaken) {
-    return res.status(409).json({ message: 'The slot is already taken...' });
-  }
-
-  const { day, seat, client, email } = req.body;
-
-  db.seats.push({ id: uuidv4(), day, seat, client, email });
-
-  req.io.emit('seatsUpdated', db.seats);
-
-  res.json({ message: 'OK' });
-});
-
-router.put('/:id', (req, res) => {
-  if (Object.keys(req.query).length === 0) {
-    return res.status(400).json({ message: 'missing data to update' });
-  }
-
-  const { id } = req.params;
-
-  const seat = db.seats.find((item) => item.id === id);
-
-  if (!seat) {
-    return res
-      .status(404)
-      .json({ message: 'this element does not exists in database' });
-  }
-
-  if (req.query.day) {
-    seat.day = Number(req.query.day);
-  }
-
-  if (req.query.seat) {
-    seat.seat = Number(req.query.seat);
-  }
-  if (req.query.client) {
-    seat.client = req.query.client;
-  }
-  if (req.query.email) {
-    seat.email = req.query.email;
-  }
-
-  db.seats = db.seats.map((item) => {
-    if (item.id === id) {
-      return seat;
-    }
-    return item;
-  });
-
-  res.json({ message: 'OK' });
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-
-  const seats = db.seats.find((item) => item.id === id);
-
-  if (!seats) {
-    return res
-      .status(404)
-      .json({ message: 'this element does not exists in database' });
-  }
-
-  db.seats = db.seats.filter((item) => item.id !== id);
-
-  res.json({ message: 'OK' });
-});
+router.delete('/:id', SeatController.deleteSeat);
 
 module.exports = router;
