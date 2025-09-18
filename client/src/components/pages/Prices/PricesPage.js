217e29012b5c697604975387a35cf95d77b7becc
Dariusz Okoński <darek200180@gmail.com>
2025-09-18 07:29:47 +0200

    display concerts from api

--- client/src/components/pages/Prices/PricesPage.js
+++ client/src/components/pages/Prices/PricesPage.js
@@ -1,24 +1,60 @@
-import { Alert, Container } from 'reactstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { Alert, Container, Progress } from 'reactstrap';
 
-const Prices = () => (
-  <Container>
-    <h1>Prices</h1>
-    <p>Prices may differ according the day of the festival. Remember that ticket includes not only the star performance, but also 10+ workshops. We gathered several genre teachers to help you increase your vocal skills, as well as self confidence.</p>
-    
-    <Alert color="info">
-        Attention! <strong>Children under 4 can go freely with you without any other fee!</strong>
-    </Alert>
+import {
+  getConcerts,
+  getRequest,
+  loadConcertsRequest,
+} from '../../../redux/concertsRedux';
+import { useEffect } from 'react';
 
-    <h2>Day one</h2>
-    <p>Price: 25$</p>
-    <p>Workshops: "Rock Music Style", "How to make you voice grooowl", "Make your voice stronger", "History of Rock"</p>
-    <h2>Day Two</h2>
-    <p>Price: 25$</p>
-    <p>Workshops: "Find your real tune", "Find your real YOU", "Fell the music", "Jam session"</p>
-    <h2>Day three</h2>
-    <p>Price: 50$</p>
-    <p>Workshops: "Increase your vocal range", "How to properly warmup before singing", "It's time for YOU!"</p>
-  </Container>
-);
+const Prices = () => {
+  const dispatch = useDispatch();
+  const concerts = useSelector(getConcerts);
+  const request = useSelector(getRequest);
+
+  useEffect(() => {
+    dispatch(loadConcertsRequest());
+  }, [dispatch]);
+
+  if (request.pending) {
+    return <Progress animated color='primary' value={50} />;
+  }
+
+  if (request.error) {
+    return <Alert color='warning'>{request.error}</Alert>;
+  }
+
+  if (!request.success || !concerts.length) {
+    return <Alert color='info'>No concerts</Alert>;
+  }
+
+  return (
+    <Container>
+      <h1>Prices</h1>
+      <p>
+        Prices may differ according the day of the festival. Remember that
+        ticket includes not only the star performance, but also 10+ workshops.
+        We gathered several genre teachers to help you increase your vocal
+        skills, as well as self confidence.
+      </p>
+
+      <Alert color='info'>
+        Attention!{' '}
+        <strong>
+          Children under 4 can go freely with you without any other fee!
+        </strong>
+      </Alert>
+
+      {concerts.map((concert) => (
+        <div>
+          <h2>Day {concert.day}</h2>
+          <p>Price: {concert.price}$</p>
+          <p>Workshop: "{concert.performer}"</p>
+        </div>
+      ))}
+    </Container>
+  );
+};
 
 export default Prices;
