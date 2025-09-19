ef38087a27b4fb49e138b97ffd85323dbcea22b2
Dariusz Okoński <darek200180@gmail.com>
2025-09-19 07:34:04 +0200

    add middleware helmet

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const socketIO = require('socket.io');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(helmet());
 app.use((req, res, next) => {
   req.io = io;
   next();
